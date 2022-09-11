acafa0d09ec22ae575d4ec2125abfbf5589be02f
Richard Clamp <richardc@unixbeard.net>
2022-09-11 18:02:19 +0100

    Day2: Part1 Calculate distance from (0,0)
    Takes a bunch of command strings.  For fanciness we implement
    Command::from so we can keep the parsing a little more distinct from the
    intepretation.  We could have skipped that and saved about 23 lines of
    code, but it felt like a good exercise in itself.

---
+++ examples/day2.rs
@@ -0,0 +1,65 @@
+enum Command {
+    Forward(i32),
+    Down(i32),
+    Up(i32),
+    Err,
+}
+
+impl From<String> for Command {
+    fn from(command: String) -> Self {
+        if let Some((verb, distance)) = command.split_once(" ") {
+            let d = distance.parse::<i32>().unwrap();
+            match verb {
+                "forward" => Command::Forward(d),
+                "down" => Command::Down(d),
+                "up" => Command::Up(d),
+                _ => Command::Err,
+            }
+        } else {
+            Command::Err
+        }
+    }
+}
+
+fn position(commands: Vec<String>) -> i32 {
+    let mut horizontal = 0;
+    let mut depth = 0;
+
+    for command in commands {
+        match Command::from(command) {
+            Command::Forward(x) => horizontal += x,
+            Command::Up(d) => depth -= d,
+            Command::Down(d) => depth += d,
+            Command::Err => {}
+        }
+    }
+
+    horizontal * depth
+}
+
+#[test]
+fn test_position() {
+    assert_eq!(
+        position(
+            vec![
+                "forward 5",
+                "down 5",
+                "forward 8",
+                "up 3",
+                "down 8",
+                "forward 2"
+            ]
+            .iter()
+            .map(|s| s.to_string())
+            .collect()
+        ),
+        150
+    );
+}
+
+use std::io;
+
+fn main() {
+    let directions: Vec<_> = io::stdin().lines().map(|s| s.unwrap()).collect();
+    println!("{}", position(directions));
+}
