a23a315d142549a1c2e1f7a1e9fd3a1a90fe7b89
Richard Clamp <richardc@unixbeard.net>
2022-09-16 02:48:04 +0100

    Day9 Part1.  Much ugliness with vectors
    Neighbours could possibly be shorter, but the checking that we were at
    the edges was important.

---
+++ examples/day9.rs
@@ -0,0 +1,82 @@
+#[derive(Debug)]
+struct Heightmap {
+    map: Vec<Vec<u8>>,
+}
+
+impl From<&[&str]> for Heightmap {
+    fn from(input: &[&str]) -> Self {
+        Heightmap {
+            map: input
+                .iter()
+                .map(|x| x.chars().map(|c| c as u8 - b'0').collect())
+                .collect(),
+        }
+    }
+}
+
+impl Heightmap {
+    fn neighbours(&self, x: usize, y: usize) -> Vec<u8> {
+        let mut res = vec![];
+        if y > 0 {
+            // above
+            res.push(self.map[x][y - 1]);
+        }
+        if x > 0 {
+            // left
+            res.push(self.map[x - 1][y]);
+        }
+        if y < self.map[0].len() - 1 {
+            // below
+            res.push(self.map[x][y + 1]);
+        }
+        if x < self.map.len() - 1 {
+            // right
+            res.push(self.map[x + 1][y]);
+        }
+        res
+    }
+
+    fn low_points(&self) -> Vec<u8> {
+        let mut low = vec![];
+        for x in 0..self.map.len() {
+            for y in 0..self.map[x].len() {
+                let val = self.map[x][y];
+                if self.neighbours(x, y).iter().all(|&x| val < x) {
+                    low.push(val)
+                }
+            }
+        }
+        low
+    }
+}
+
+#[test]
+fn test_low_points() {
+    let example = r#"
+2199943210
+3987894921
+9856789892
+8767896789
+9899965678
+"#;
+
+    let input = example.trim().split('\n').collect::<Vec<_>>();
+    let slice: &[&str] = &input;
+    let map = Heightmap::from(slice);
+    assert_eq!(map.low_points(), [1, 0, 5, 5]);
+    assert_eq!(risk_level(map), 15);
+}
+
+fn risk_level(heightmap: Heightmap) -> u32 {
+    heightmap.low_points().iter().map(|&x| x as u32 + 1).sum()
+}
+
+use std::io;
+
+fn main() {
+    let lines = io::stdin().lines().map(|s| s.unwrap()).collect::<Vec<_>>();
+    let input = lines.iter().map(|x| x.as_str()).collect::<Vec<_>>();
+    let slice: &[&str] = &input;
+
+    println!("{}", risk_level(Heightmap::from(slice)));
+}
