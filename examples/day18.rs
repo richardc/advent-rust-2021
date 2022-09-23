137ba19f9564bcc86892dce6e6cfb6ac3bbacd4e
Richard Clamp <richardc@unixbeard.net>
2022-09-23 12:09:15 +0100

    Add Pair.reduce, Pair.explode, Pair.split
    Pair.explode isn't currently implemented, but we wrote the tests out for
    it.

--- examples/day18.rs
+++ examples/day18.rs
@@ -10,7 +10,7 @@ use nom::{
 
 type Number = u32;
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 enum Pair {
     Number(Number),
     Pair(Box<Self>, Box<Self>),
@@ -137,6 +137,86 @@ fn test_pair_add() {
     assert_eq!(
         Pair::from("[1,2]") + Pair::from("[[3,4],5]"),
         "[[1,2],[[3,4],5]]"
+    );
+}
+
+impl Pair {
+    fn explode(self) -> Self {
+        self
+    }
+}
+
+#[test]
+fn test_pair_explode() {
+    assert_eq!(
+        Pair::from("[[[[[9,8],1],2],3],4]").explode(),
+        "[[[[0,9],2],3],4]"
+    );
+
+    assert_eq!(
+        Pair::from("[7,[6,[5,[4,[3,2]]]]]").explode(),
+        "[7,[6,[5,[7,0]]]]"
+    );
+
+    assert_eq!(
+        Pair::from("[[6,[5,[4,[3,2]]]],1]").explode(),
+        "[[6,[5,[7,0]]],3]"
+    );
+
+    assert_eq!(
+        Pair::from("[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]").explode(),
+        "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]"
+    );
+
+    assert_eq!(
+        Pair::from("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]").explode(),
+        "[[3,[2,[8,0]]],[9,[5,[7,0]]]]"
+    );
+}
+
+impl Pair {
+    fn split(self) -> Self {
+        match self {
+            Pair::Number(x) if x > 9 => Pair::Pair(
+                Box::new(Pair::Number(x / 2)),
+                Box::new(Pair::Number((x + 1) / 2)),
+            ),
+            Pair::Pair(left, right) => Pair::Pair(Box::new(left.split()), Box::new(right.split())),
+            _ => self,
+        }
+    }
+}
+
+#[test]
+fn test_pair_split() {
+    assert_eq!(Pair::from("[0,9]").split(), "[0,9]");
+    assert_eq!(Pair::from("[0,10]").split(), "[0,[5,5]]");
+    assert_eq!(Pair::from("[0,11]").split(), "[0,[5,6]]");
+    assert_eq!(Pair::from("[0,12]").split(), "[0,[6,6]]");
+}
+
+impl Pair {
+    fn reduce(self) -> Self {
+        let old = self.clone();
+
+        let exploded = self.clone().explode();
+        if exploded != old {
+            return exploded.reduce();
+        }
+
+        let split = self.clone().split();
+        if split != old {
+            return split.reduce();
+        }
+        return self;
+    }
+}
+
+#[test]
+fn test_pair_reduce() {
+    assert_eq!(
+        (Pair::from("[[[[4,3],4],4],[7,[[8,4],9]]]") + Pair::from("[1,1]")).reduce(),
+        "[[[[0,7],4],[[7,8],[6,0]]],[8,1]]"
     );
 }
 
