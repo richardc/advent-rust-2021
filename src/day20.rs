63e099568609a8301ff8887bde429b1da9e9c777
Richard Clamp <richardc@unixbeard.net>
2022-09-27 12:13:34 +0100

    Day20 Part1: Track what the infinite void looks like
    To the image we add more state, the bit value for the background and the
    known bounds.  After computing the new points, figure out what the
    background byte will be by checking the value for some place
    out-of-bounds.  When fetching the bit value check if it's in or out of
    known bounds, when out of bounds it's 'background', otherwise it's '1',
    or '0' depending on if the value has been set.

--- src/day20.rs
+++ src/day20.rs
@@ -4,6 +4,11 @@ use itertools::Itertools;
 
 #[derive(Debug, PartialEq, Clone)]
 struct Image {
+    background: char,
+    x_min: i32,
+    x_max: i32,
+    y_min: i32,
+    y_max: i32,
     algorithm: [u8; 512],
     pixels: HashSet<[i32; 2]>,
 }
@@ -36,7 +41,17 @@ fn generate(input: &str) -> Image {
         }
     }
 
-    Image { algorithm, pixels }
+    let mut image = Image {
+        background: '0',
+        algorithm,
+        pixels,
+        x_min: 0,
+        x_max: 0,
+        y_min: 0,
+        y_max: 0,
+    };
+    image.set_bounds();
+    image
 }
 
 #[test]
@@ -58,7 +73,9 @@ impl Image {
     fn neighbour_string(&self, x: i32, y: i32) -> String {
         let mut bits = String::with_capacity(9);
         for (y, x) in iproduct!((y - 1)..=(y + 1), (x - 1)..=(x + 1)) {
-            if self.pixels.contains(&[x, y]) {
+            if x < self.x_min || x > self.x_max || y < self.y_min || y > self.y_max {
+                bits.push(self.background)
+            } else if self.pixels.contains(&[x, y]) {
                 bits.push('1');
             } else {
                 bits.push('0');
@@ -70,38 +87,63 @@ impl Image {
 
 #[test]
 fn test_neighbour_string() {
-    let image = generate(include_str!("day20_example.txt"));
-    assert_eq!(image.neighbour_string(-1000, -1000), "000000000");
+    let mut image = generate(include_str!("day20_example.txt"));
     assert_eq!(image.neighbour_string(0, 0), "000010010");
     assert_eq!(image.neighbour_string(2, 2), "000100010");
+
+    // When out of bounds, we should use the background char, which defaults to 0
+    assert_eq!(image.neighbour_string(-1000, -1000), "000000000");
+    image.background = '1';
+    assert_eq!(image.neighbour_string(-1000, -1000), "111111111");
 }
 
 impl Image {
-    fn step(&mut self) {
-        let (&x_min, &x_max) = self
+    fn set_bounds(&mut self) {
+        let (x_min, x_max) = self
             .pixels
             .iter()
             .map(|[x, _]| x)
             .minmax()
             .into_option()
             .unwrap();
-        let (&y_min, &y_max) = self
+        self.x_min = *x_min;
+        self.x_max = *x_max;
+
+        let (y_min, y_max) = self
             .pixels
             .iter()
             .map(|[_, y]| y)
             .minmax()
             .into_option()
             .unwrap();
+        self.y_min = *y_min;
+        self.y_max = *y_max;
+    }
+}
+
+impl Image {
+    fn step(&mut self) {
         let mut next = HashSet::new();
 
-        for (x, y) in iproduct!(x_min - 1..=x_max + 1, y_min - 1..=y_max + 1) {
+        for (x, y) in iproduct!(
+            self.x_min - 1..=self.x_max + 1,
+            self.y_min - 1..=self.y_max + 1
+        ) {
             let index = usize::from_str_radix(&self.neighbour_string(x, y), 2).unwrap();
             if self.algorithm[index] == 1 {
                 next.insert([x, y]);
             }
         }
 
+        // calculate next background value
+        let index = usize::from_str_radix(
+            &self.neighbour_string(self.x_max + 1000, self.y_max + 1000),
+            2,
+        )
+        .unwrap();
+        self.background = if self.algorithm[index] == 1 { '1' } else { '0' };
         self.pixels = next;
+        self.set_bounds();
     }
 }
 
