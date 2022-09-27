79dc5534972e1241e1cdeeb71c82a69cc8038e29
Richard Clamp <richardc@unixbeard.net>
2022-09-27 21:11:42 +0100

    Day 22: Part 1: Simulate the reactor bootup
    Brings in the `euclid` crate so we have something in Box3D that does the
    intersection for us.  Could implement it ourselves, but it's a bunch of
    LoC that don't add value.

---
+++ src/day22.rs
@@ -0,0 +1,117 @@
+use std::collections::HashSet;
+
+use euclid::{box3d, default::Box3D};
+use itertools::iproduct;
+use lazy_static::lazy_static;
+use regex::Regex;
+
+fn parse_box(input: &str) -> Box3D<i32> {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"x=(?P<x1>-?\d+)..(?P<x2>-?\d+),y=(?P<y1>-?\d+)..(?P<y2>-?\d+),z=(?P<z1>-?\d+)..(?P<z2>-?\d+)")
+        .unwrap();
+    }
+    let captures = RE.captures(input).unwrap();
+
+    box3d(
+        captures["x1"].parse().unwrap(),
+        captures["y1"].parse().unwrap(),
+        captures["z1"].parse().unwrap(),
+        captures["x2"].parse::<i32>().unwrap() + 1,
+        captures["y2"].parse::<i32>().unwrap() + 1,
+        captures["z2"].parse::<i32>().unwrap() + 1,
+    )
+}
+
+enum Operation {
+    On,
+    Off,
+}
+struct Instruction {
+    operation: Operation,
+    region: Box3D<i32>,
+}
+
+impl From<&str> for Instruction {
+    fn from(input: &str) -> Self {
+        let (op, region) = input.split_once(' ').unwrap();
+        Instruction {
+            region: parse_box(region),
+            operation: match op {
+                "on" => Operation::On,
+                "off" => Operation::Off,
+                _ => unreachable!(),
+            },
+        }
+    }
+}
+
+#[derive(Clone)]
+struct Reactor {
+    bounds: Box3D<i32>,
+    cells: HashSet<[i32; 3]>,
+}
+
+impl Reactor {
+    fn new() -> Self {
+        Self {
+            bounds: parse_box("x=-50..50,y=-50..50,z=-50..50"),
+            cells: HashSet::new(),
+        }
+    }
+}
+
+impl Reactor {
+    fn apply(&mut self, instruction: &Instruction) {
+        if let Some(bounds) = self.bounds.intersection(&instruction.region) {
+            for (x, y, z) in iproduct!(bounds.x_range(), bounds.y_range(), bounds.z_range()) {
+                match instruction.operation {
+                    Operation::On => self.cells.insert([x, y, z]),
+                    Operation::Off => self.cells.remove(&[x, y, z]),
+                };
+            }
+        }
+    }
+}
+
+#[test]
+fn test_instruction_apply() {
+    let mut reactor = Reactor::new();
+    reactor.apply(&Instruction::from("on x=10..12,y=10..12,z=10..12"));
+    assert_eq!(reactor.cells.len(), 27);
+
+    reactor.apply(&Instruction::from("on x=11..13,y=11..13,z=11..13"));
+    assert_eq!(reactor.cells.len(), 27 + 19);
+
+    reactor.apply(&Instruction::from("off x=9..11,y=9..11,z=9..11"));
+    assert_eq!(reactor.cells.len(), 27 + 19 - 8);
+
+    reactor.apply(&Instruction::from("on x=10..10,y=10..10,z=10..10"));
+    assert_eq!(reactor.cells.len(), 39);
+
+    reactor.apply(&Instruction::from(
+        "on x=-54112..-39298,y=-85059..-49293,z=-27449..7877",
+    ));
+    assert_eq!(reactor.cells.len(), 39);
+}
+
+#[aoc_generator(day22)]
+fn generate(input: &str) -> Vec<Instruction> {
+    input.lines().map(Instruction::from).collect()
+}
+
+#[aoc(day22, part1)]
+fn lit_cubes(input: &[Instruction]) -> usize {
+    let mut reactor = Reactor::new();
+    for instruction in input {
+        reactor.apply(&instruction);
+    }
+    reactor.cells.len()
+}
+
+#[test]
+fn test_lit_cubes() {
+    assert_eq!(
+        lit_cubes(&generate(include_str!("day22_example.txt"))),
+        590784
+    );
+}
