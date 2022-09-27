9274a5fa3c89f405c72742df97f57e5eb3361dde
Richard Clamp <richardc@unixbeard.net>
2022-09-27 15:33:55 +0100

    Day 21: Part 2: recurse through the universes
    So this time we are doing something more mathy, just counting the
    universes in which each player won, given the probabilites of them
    making each roll.
    
    We do this by recursing down the tree of gamestates
    (position,score,next_player) and multiplying out what we find.

--- src/day21.rs
+++ src/day21.rs
@@ -1,4 +1,4 @@
-use std::cmp::min;
+use std::cmp::{max, min};
 
 use itertools::Itertools;
 
@@ -107,4 +107,61 @@ fn losing_factor(game: &Game) -> i32 {
     let mut game = (*game).clone();
     game.play_till_won();
     min(game.scores[0], game.scores[1]) * game.rolls
+}
+
+fn make_move(
+    positions: [i32; 2],
+    scores: [i32; 2],
+    roll: i32,
+    to_play: usize,
+) -> ([i32; 2], [i32; 2]) {
+    let mut pos = positions;
+    let mut score = scores;
+
+    pos[to_play] += roll;
+    pos[to_play] %= 10;
+
+    score[to_play] += pos[to_play] + 1;
+
+    (pos, score)
+}
+
+use memoize::memoize;
+#[memoize]
+fn wins_in_space(positions: [i32; 2], scores: [i32; 2], to_play: usize) -> (u64, u64) {
+    // Base case, this is a won game for player 1
+    if scores[0] >= 21 {
+        return (1, 0);
+    }
+
+    // player 2 win
+    if scores[1] >= 21 {
+        return (0, 1);
+    }
+
+    let mut winners = (0, 0);
+    // When we roll 3 Dirac dice, we get one of the following total rolls at each
+    // given frequency, so we recurse just on the roll totals and current game state.
+    for [roll, frequency] in [[3, 1], [4, 3], [5, 6], [6, 7], [7, 6], [8, 3], [9, 1]] {
+        let (positions, scores) = make_move(positions, scores, roll, to_play);
+        let wins = wins_in_space(positions, scores, 1 - to_play);
+
+        // Now we know who won in that branch of the game, multiply it back up
+        // by frequency to count the number of universes that happened in.
+        winners.0 += wins.0 * frequency as u64;
+        winners.1 += wins.1 * frequency as u64;
+    }
+    winners
+}
+
+#[aoc(day21, part2)]
+fn winning_universes(game: &Game) -> u64 {
+    let (p1, p2) = wins_in_space(game.positions, game.scores, 0);
+
+    max(p1, p2)
+}
+
+#[test]
+fn test_winning_universes() {
+    assert_eq!(winning_universes(&Game::new(3, 7)), 444356092776315);
 }
