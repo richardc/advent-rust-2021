30266f3f8668a5f6b5e6c0897935414c88db025d
Richard Clamp <richardc@unixbeard.net>
2022-09-28 10:25:21 +0100

    Day 22: Part 2 - track lit cubes
    Rather than simulating the actual lights, we really need to consider the
    lit up ranges as a collection cubes.  For each new command, we check if
    it intersects with any of the known lit cubes, and if so we slice the
    existing cube up into it's 27 subcubes (cubelets?) and discare the
    intersection.
    
    Over time this'll mean we end up with a lot of very small cubes
    represented - we could go the extra step to fuse them back together to
    avoid the complexity of this exploding, but since we only have 420
    commands in our inputs we're only talking about tracking 10,490 cubes in
    the worst case.

--- src/day22.rs
+++ src/day22.rs
@@ -1,11 +1,11 @@
 use std::collections::HashSet;
 
 use euclid::{box3d, default::Box3D};
-use itertools::iproduct;
+use itertools::{iproduct, Itertools};
 use lazy_static::lazy_static;
 use regex::Regex;
 
-fn parse_box(input: &str) -> Box3D<i32> {
+fn parse_box(input: &str) -> Box3D<i64> {
     lazy_static! {
         static ref RE: Regex = Regex::new(r"x=(?P<x1>-?\d+)..(?P<x2>-?\d+),y=(?P<y1>-?\d+)..(?P<y2>-?\d+),z=(?P<z1>-?\d+)..(?P<z2>-?\d+)")
         .unwrap();
@@ -16,19 +16,20 @@ fn parse_box(input: &str) -> Box3D<i32> {
         captures["x1"].parse().unwrap(),
         captures["y1"].parse().unwrap(),
         captures["z1"].parse().unwrap(),
-        captures["x2"].parse::<i32>().unwrap() + 1,
-        captures["y2"].parse::<i32>().unwrap() + 1,
-        captures["z2"].parse::<i32>().unwrap() + 1,
+        captures["x2"].parse::<i64>().unwrap() + 1,
+        captures["y2"].parse::<i64>().unwrap() + 1,
+        captures["z2"].parse::<i64>().unwrap() + 1,
     )
 }
 
+#[derive(PartialEq)]
 enum Operation {
     On,
     Off,
 }
 struct Instruction {
     operation: Operation,
-    region: Box3D<i32>,
+    region: Box3D<i64>,
 }
 
 impl From<&str> for Instruction {
@@ -47,14 +48,12 @@ impl From<&str> for Instruction {
 
 #[derive(Clone)]
 struct Reactor {
-    bounds: Box3D<i32>,
-    cells: HashSet<[i32; 3]>,
+    cells: HashSet<[i64; 3]>,
 }
 
 impl Reactor {
     fn new() -> Self {
         Self {
-            bounds: parse_box("x=-50..50,y=-50..50,z=-50..50"),
             cells: HashSet::new(),
         }
     }
@@ -66,13 +65,15 @@ impl Reactor {
 
 impl Reactor {
     fn apply(&mut self, instruction: &Instruction) {
-        if let Some(bounds) = self.bounds.intersection(&instruction.region) {
-            for (x, y, z) in iproduct!(bounds.x_range(), bounds.y_range(), bounds.z_range()) {
-                match instruction.operation {
-                    Operation::On => self.cells.insert([x, y, z]),
-                    Operation::Off => self.cells.remove(&[x, y, z]),
-                };
-            }
+        for (x, y, z) in iproduct!(
+            instruction.region.x_range(),
+            instruction.region.y_range(),
+            instruction.region.z_range()
+        ) {
+            match instruction.operation {
+                Operation::On => self.cells.insert([x, y, z]),
+                Operation::Off => self.cells.remove(&[x, y, z]),
+            };
         }
     }
 }
@@ -118,5 +119,116 @@ fn test_lit_initialized_cubes() {
     assert_eq!(
         lit_initialized_cubes(&generate(include_str!("day22_example.txt"))),
         590784
+    );
+}
+
+#[aoc(day22, part1, slicing)]
+fn lit_initialized_cubes_slicing(input: &[Instruction]) -> usize {
+    let mut reactor = SlicingReactor::new();
+
+    // Only consider operations on our core
+    let bounds = parse_box("x=-50..50,y=-50..50,z=-50..50");
+
+    input
+        .iter()
+        .filter(|i| bounds.contains_box(&i.region))
+        .for_each(|i| reactor.apply(i));
+
+    reactor.lit_cubes()
+}
+
+#[test]
+fn test_lit_initialized_cubes_slicing() {
+    assert_eq!(
+        lit_initialized_cubes_slicing(&generate(include_str!("day22_example.txt"))),
+        590784
+    );
+}
+
+#[derive(Default)]
+struct SlicingReactor {
+    lit_regions: Vec<Box3D<i64>>,
+}
+
+fn slice_cube(cube: &Box3D<i64>, cut: &Box3D<i64>) -> Vec<Box3D<i64>> {
+    let mut result = vec![];
+    // Generate cubelets by slicing the cube by its intersecting cut.
+    let xs = [cube.max.x, cube.min.x, cut.max.x, cut.min.x]
+        .into_iter()
+        .sorted()
+        .tuple_windows::<(_, _)>();
+
+    let ys = [cube.max.y, cube.min.y, cut.max.y, cut.min.y]
+        .into_iter()
+        .sorted()
+        .tuple_windows::<(_, _)>();
+
+    let zs = [cube.max.z, cube.min.z, cut.max.z, cut.min.z]
+        .into_iter()
+        .sorted()
+        .tuple_windows::<(_, _)>();
+
+    for ((x1, x2), (y1, y2), (z1, z2)) in iproduct!(xs, ys, zs) {
+        let cubelet = box3d(x1, y1, z1, x2, y2, z2);
+        // Lose the cutting tool
+        if *cut != cubelet {
+            result.push(cubelet);
+        }
+    }
+
+    result
+}
+
+#[test]
+fn test_slice_cube() {
+    assert_eq!(
+        slice_cube(&box3d(0, 0, 0, 10, 10, 10), &box3d(0, 0, 0, 1, 1, 1)).len(),
+        26,
+    );
+}
+
+impl SlicingReactor {
+    fn new() -> Self {
+        Self::default()
+    }
+
+    fn apply(&mut self, instruction: &Instruction) {
+        let mut regions = vec![];
+
+        for lit in &self.lit_regions {
+            if let Some(intersection) = lit.intersection(&instruction.region) {
+                slice_cube(lit, &intersection).iter().for_each(|b| {
+                    regions.push(*b);
+                });
+            } else {
+                regions.push(*lit);
+            }
+        }
+
+        if instruction.operation == Operation::On {
+            regions.push(instruction.region)
+        }
+        self.lit_regions = regions;
+    }
+
+    fn lit_cubes(&self) -> usize {
+        self.lit_regions.iter().map(|b| b.volume() as usize).sum()
+    }
+}
+
+#[aoc(day22, part2)]
+fn lit_all_cubes(input: &[Instruction]) -> usize {
+    let mut reactor = SlicingReactor::new();
+
+    input.iter().for_each(|i| reactor.apply(i));
+
+    reactor.lit_cubes()
+}
+
+#[test]
+fn test_lit_all_cubes() {
+    assert_eq!(
+        lit_all_cubes(&generate(include_str!("day22_example_2.txt"))),
+        2758514936282235
     );
 }
